a667e5b46930fff61f8c0c572789914685e8caaa
KuromiK <98142015+KuromiK@users.noreply.github.com>
2022-02-25 21:40:56 +0300

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,5 +1,3 @@
-id = str(input())
-
 res = worksheet.col_values(1)
 res1 = worksheet.col_values(2)
 
@@ -7,10 +5,6 @@ def faker():
     if clientid != res1:
         if abonid != res:
             print('Внимание, поддельный штрих-код')
-def valid():
-    faker()
-else:
-    print("Не удалось распознать штрих-код")
 
 if __name__ == '__main__':
-    print(id)
+    faker()
