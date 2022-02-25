a78c32180fe8f84c9b18a46034216d8ea53a1843
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2022-02-25 21:15:52 +0300

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,18 +1,21 @@
 id = str(input())
 
-if len(id) >= 3 and len(id) < 8:
-    tire = id.find("-")
-    if tire == -1:
-        print('Не удалось распознать штрих-код')
-    else:
-        a = id[:tire]
-        b = id[(tire + 1):]
-        if len(a) == len(b) == 3:
-            if b not in res:
-                print('Неизвестный абонемент')
+res = worksheet.col_values(1)
 
-else:
-    print("Не удалось распознать штрих-код")
+def unknowing():
+    if abonid not in res:
+        print('Неизвестный абонемент')
+def valid():
+    if len(id) >= 3:
+        tire = id.find("-")
+        if tire == -1:
+            print('Не удалось распознать штрих-код')
+        else:
+            clientid = id[:tire]
+            abonid = id[(tire+1):]
+            unknowing()  
+    else:
+        print("Не удалось распознать штрих-код")
 
 if __name__ == '__main__':
-    print(id)
+    valid()
