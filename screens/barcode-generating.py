5534f9ece3ee391744abbdf52177216a9b69731a
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2022-03-11 22:59:58 +0300

    Update barcode-generating.py

--- screens/barcode-generating.py
+++ screens/barcode-generating.py
@@ -1,10 +1,31 @@
 from barcode import Code128
 from barcode.writer import ImageWriter
+from gspread import spreadsheet
+import os
+
+clientid = 0
+abonid = 0
 
 def Barcoding():
+    global clientid, abonid
+    res = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Список, из которого будет браться ИД клиента
+    res1 = [1, 3, 9, 10, 11, 15, 16, 18, 20]  # Список, из которого будет браться ИД абонемента
+    resname = ["ЗАС", "ЯСФ", "ГВВ",
+               "АУА", "МЕВ", "ЗАС",
+               "ДАА", "NULL", "NULL",
+               "ЗАС"]  # Список с именами в качестве значений, по которым ведётся поиск для ИД клиента/абонемента
+    for i in range(len(resname)-1, 1, -1):
+        if resname[i] == "ГВВ":
+            clientid = res[i]  # ИД клиента, который берётся из списка ИД клиентов
+            abonid = res1[i]  # ИД абонемента, который берётся из списка абонементов
+            break
     with open('somefile.jpeg', 'wb') as f:
-        clienid = 0
-        abonid = 0
-        Code128(f'{clienid}-{abonid}', writer=ImageWriter).write(f)
+        Code128(f'{clientid}-{abonid}', writer=ImageWriter()).write(f)
 
-Barcoding()
+def BarcodeDelete():
+    os.remove('somefile.jpeg')
+
+if __name__ == "__main__":
+    Barcoding()
+    #Здесь будет вызвана функция "Отправить штрих-код"
+    #BarcodeDelete() функция удаляет файл после выполнения предыдущих функций
